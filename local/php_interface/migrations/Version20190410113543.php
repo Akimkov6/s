33a1b1d4c6f5c7bfbf10f4e5755c4a3364c0a639
Kate K <you@example.com>
2019-04-18 11:01:54 +0300

    add migration

--- local/php_interface/migrations/Version20190410113543.php
+++ local/php_interface/migrations/Version20190410113543.php
@@ -5,7 +5,7 @@ namespace Sprint\Migration;
 
 class Version20190410113543 extends Version
 {
-    protected $description = 'Создание таблицы для истории названий лотов';
+    protected $description = 'Создание таблицы для секретов и доступов';
 
     private $a1_secret = 'a1_secret';
     private $a1_dostyp ='a1_dostyp';
@@ -14,36 +14,35 @@ class Version20190410113543 extends Version
     {
         global $DB;
 
-        $sql = $this->getSQLCreateTableSecret();
-        $DB->Query($sql);
+		$sql = $this->getSQLCreateTableSecret();
+$DB->Query($sql);
         $sql = $this->getSQLCreateTableDostyp();
         $DB->Query($sql);
     }
 
     public function down()
     {
-        global $DB;
-
+		global $DB;
         $sql = "DROP TABLE IF EXISTS `{$this->a1_secret}`, `{$this->a1_dostyp}`";
-        $DB->Query($sql);
+$DB->Query($sql);
     }
 
     private function getSQLCreateTableSecret()
     {
-    return "CREATE TABLE IF NOT EXISTS `{$this->a1_secret}`(
-    `id` MEDIUMINT NOT NULL AUTO_INCREMENT NOT NULL,
+	return "CREATE TABLE IF NOT EXISTS `{$this->a1_secret}`(
+    `id` INT NOT NULL AUTO_INCREMENT NOT NULL,
     `name` varchar(255) NOT NULL,
     `text` varchar(1023) NOT NULL,
      PRIMARY KEY (id));
-     ";
+";
     }
 
     private function getSQLCreateTableDostyp()
     {
     return "CREATE TABLE IF NOT EXISTS `{$this->a1_dostyp}`(
-    `id` MEDIUMINT NOT NULL AUTO_INCREMENT NOT NULL,
-    `secret_id` MEDIUMINT NOT NULL,
-    `user_id` MEDIUMINT NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT NOT NULL,
+    `secret_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
      PRIMARY KEY (id));
      ";
     }
